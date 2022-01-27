35a5deee7bf086895901568d0c6143c73f70df06
Luis Reyes <albertoreyestecampo@gmail.com>
2022-01-27 00:01:10 -0600

    Undertand filter & mapping

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -18,6 +18,12 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    List<Car> carsFiltered = cars.stream()
+            .filter(car -> car.getPrice() < 7000)
+            .collect(Collectors.toList());
+
+    carsFiltered.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
 
   }
 
@@ -25,17 +31,31 @@ public class Lecture5 {
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
+    List<PersonDTO> personDTO = people.stream()
+            .map(person ->  new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+            .collect(Collectors.toList());
 
+    personDTO.forEach(System.out::println);
+    assertThat(personDTO).hasSize(1000);
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
+    ImmutableList<Car> cars = MockData.getCars();
+    double average = cars.stream()
+            .mapToDouble(Car::getPrice)
+            .average()
+            .orElse(0);
+
+    System.out.println(average);
+
 
   }
 
   @Test
   public void test() throws Exception {
+    MockData.getCars().forEach(System.out::println);
 
   }
 }
