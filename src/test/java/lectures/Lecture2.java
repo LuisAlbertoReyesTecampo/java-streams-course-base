598dbd708c52a16d3ad157fc0b16aa6801083502
Luis Reyes <albertoreyestecampo@gmail.com>
2022-01-26 23:12:12 -0600

    Iterating people list, iterate method

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,12 +10,27 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
-
+    for( int i = 0; i <= 10 ; i++){
+      System.out.println(i);
+    }
+    System.out.println("Exclusive");
+    IntStream.range(0,10)
+            //.forEach(index -> System.out.println(index));
+            .forEach(System.out::println);
+    System.out.println("Inclusive");
+    IntStream.rangeClosed(0,10).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
+
+    IntStream.range(0,people.size())
+            .forEach(index -> {
+              Person person = people.get(index);
+              System.out.println(person);
+            });
+    people.forEach(System.out::println);
 
   }
 
@@ -25,6 +40,9 @@ public class Lecture2 {
           .filter(number -> number % 2 == 0)
           .limit(20)
           .forEach(System.out::println);*/
-
+    IntStream.iterate(0, operand -> operand + 1)
+            .filter(number -> number % 2 == 0 ) // even numbers
+            .limit(20)
+            .forEach(System.out::println);
   }
 }
